22a237521563d3123ba8fcd9f3a12003f9d85eb6
Lucas <lucasatletico@hotmail.com>
2020-03-21 10:08:50 -0300

    Criando construtores

--- src/cursojava/classe/Produto.java
+++ src/cursojava/classe/Produto.java
@@ -4,4 +4,19 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	Produto(String nomeInicial){
+		nome = nomeInicial;
+	}
+	Produto(String nomeInicial, double precoInicial, double descontoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+		
+	}
+	
+	double precoComDesconto(double descontoGerente) {
+		return preco * (1 - desconto + descontoGerente);
+				
+	}
 }
