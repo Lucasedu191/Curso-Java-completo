1465a59e0841ed6b1c12f85328c9884c090a7343
Lucas <lucasatletico@hotmail.com>
2020-03-25 15:55:16 -0300

    desafio composicao orientação a objetos

---
+++ src/oo/composicao/desafio/Cliente.java
@@ -0,0 +1,9 @@
+package oo.composicao.desafio;
+
+
+	public class Cliente {
+	public static void main(String[] args) {
+		Compra compra1 = new Compra();
+		compra1,adicionarItem("Caneta", 1, 100);
+	}	
+}
