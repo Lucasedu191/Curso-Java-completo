f638e282d61ac5c5534616414be9342805ae783f
Lucas <lucasatletico@hotmail.com>
2020-03-28 11:38:56 -0300

    orientação a objetos desafio acelerar e frear carro

---
+++ src/oo/heranca/desafio/CarroTeste.java
@@ -0,0 +1,27 @@
+package oo.heranca.desafio;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Civic c1 = new Civic();
+		
+		c1.acelerar();
+		System.out.println(c1);
+		c1.acelerar();
+		System.out.println(c1);
+		c1.acelerar();
+		System.out.println(c1);
+		
+		Ferrari c2 = new Ferrari();
+		
+
+		c2.acelerar();
+		System.out.println(c2);
+		c2.acelerar();
+		System.out.println(c2);
+		c2.acelerar();
+		System.out.println(c2);
+	}
+
+}
